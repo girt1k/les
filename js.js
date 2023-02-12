2c7871dacfe8a7c7e5f7ae15cc27284433546f18
girt1k <e_kirillov@yahoo.com>
2023-02-12 18:42:35 +0300

    push

---
+++ js.js
@@ -0,0 +1,8 @@
+function run() {
+    let htmlCode = document.getElementById("html-code")
+    let cssCode = document.getElementById("css-code")
+    let jsCode = document.getElementById("js-code")
+    let output = document.getElementById("output")
+
+    output.contentDocument.body.innerHTML = htmlCode + "<style>" + cssCode + "</style>"
+}
